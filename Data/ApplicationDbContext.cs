09d527fb3cfbe8d9132708160c7b3e4df91125ff
sgtadt <sgtadt@gmail.com>
2025-08-09 14:13:46 -0500

    fleshing out the auth for production readiness

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
-//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-//namespace BellwoodAuthServer.Data
-//{
-//    public class ApplicationDbContext : IdentityDbContext
-//    {
-//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-//            : base(options) { }
-//    }
-//}
+namespace BellwoodAuthServer.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+    }
+}
